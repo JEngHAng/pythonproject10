fa4474412a1de61d32f0bc231ab1b66f1d11b7c7
JEngHAng <jangg500z@gmail.com>
2023-11-10 10:42:50 +0700

    extend

--- test03.py
+++ test03.py
@@ -5,11 +5,14 @@
 # อ่านข้อมูลในไฟล์ คือ input
 # การเปิดไฟล์ write (w) / extend (x) / read (r)
 
-f_iot = open ("iot4.txt" , "x" , encoding="utf-8") # ถ้าไม่มีไฟล์มันจะสร้างใหม่
+try :
+    f_iot = open ("iot4.txt" , "x" , encoding="utf-8") # ถ้าไม่มีไฟล์มันจะสร้างใหม่
 
-f_iot.write ("hello na ja")
-f_iot.write ("hello naaa jaaa\n")
-f_iot.write ("hello naaaa jaaaa\n")
-f_iot.write ("HI kub\n")
+    f_iot.write ("hello na ja")
+    f_iot.write ("hello naaa jaaa\n")
+    f_iot.write ("hello naaaa jaaaa\n")
+    f_iot.write ("HI kub\n")
 
-f_iot.close()
+    f_iot.close()
+except FileExistsError :
+    print ("กรุณาเปลี่ยนชื่อไฟล์ เพราะ ซ้ำ .... --")
